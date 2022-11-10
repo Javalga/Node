e6d392a12d7dc28860fe2950f7cb6633fb1a62fa
Javier Alonso <jalonsogal@gmail.com>
2022-11-10 14:41:23 +0100

    a

---
+++ promises_async/reto2/asyncAwait/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs/promises");
+
+let newObj = {
+  name: "Javi",
+  surname: "Alonso",
+  edad: 22,
+};
+
+const readAndWrite = async (currentItem, fileName) => {
+  let stringifiedObj = JSON.stringify(currentItem, null, 2);
+  try {
+    await fs.writeFile(`./${fileName}.json`, stringifiedObj);
+    const data = await fs.readFile(`./${fileName}.json`, "utf8");
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+readAndWrite(newObj, "newObj");
